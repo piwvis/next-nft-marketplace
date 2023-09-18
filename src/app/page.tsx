5e20711fa4d3858abc19cc76106cc422e5cd455c
fastgonzalez <fastergonzalez@proton.me>
2023-09-18 17:43:36 +0200

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,234 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { Contract, ethers } from "ethers";
+import { SetStateAction, useEffect, useState } from "react";
+import contractABI from "../contractABI.json";
+import { Button } from "~/components/ui/button";
+
+const contractAddress = "0x1580260Be8Db6da9BFEaEc0a5B205DB7253BF125";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+  const [account, setAccount] = useState(null);
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  const [NFTContract, setNFTContract] = useState<Contract | null>(null);
+  // state for whether app is minting or not.
+  const [isMinting, setIsMinting] = useState(false);
+  const [isCorrectNetwork, setIsCorrectNetwork] = useState(false); // Added state for network check
+
+  useEffect(() => {
+    async function checkNetwork() {
+      if (window.ethereum) {
+        const chainId = await window.ethereum.request({
+          method: "eth_chainId",
+        });
+        setIsCorrectNetwork(chainId === "0x13881"); // Change to the correct chain ID
+      }
+    }
+    //check for initial network
+    checkNetwork();
+
+    //Check for network change
+    window.ethereum.on(
+      "chainChanged",
+      (newChainId: string) => {
+        setIsCorrectNetwork(newChainId === "0x13881"); // Change to the correct chain ID
+      },
+      []
+    );
+  }, []);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    function initNFTContract() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setNFTContract(new Contract(contractAddress, contractABI.abi, signer));
+    }
+    initNFTContract();
+  }, [account]);
+
+  async function connectWallet() {
+    window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts: SetStateAction<null>[]) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error: any) => {
+        alert("Something went wrong");
+      });
+  }
+
+  async function disconnectWallet() {
+    if (window.ethereum) {
+      try {
+        setAccount(null);
+      } catch (error) {
+        console.error(
+          "An error occurred while disconnecting the wallet:",
+          error
+        );
+      }
+    }
+  }
+
+  const data = [
+    {
+      url: "/assets/images/1.png",
+      param:
+        "handleMint('https://magenta-multiple-leech-484.mypinata.cloud/ipfs/QmXZ3TgRgd5EZEk2DhwGvjf8f6sQJNCrnHzrEw1oHufgnL/1.png')",
+    },
+    {
+      url: "/assets/images/2.png",
+      param:
+        "handleMint('https://magenta-multiple-leech-484.mypinata.cloud/ipfs/QmXZ3TgRgd5EZEk2DhwGvjf8f6sQJNCrnHzrEw1oHufgnL/2.png')",
+    },
+    {
+      url: "/assets/images/3.png",
+      param:
+        "handleMint('https://magenta-multiple-leech-484.mypinata.cloud/ipfs/QmXZ3TgRgd5EZEk2DhwGvjf8f6sQJNCrnHzrEw1oHufgnL/3.png')",
+    },
+    {
+      url: "/assets/images/4.png",
+      param:
+        "handleMint('https://magenta-multiple-leech-484.mypinata.cloud/ipfs/QmXZ3TgRgd5EZEk2DhwGvjf8f6sQJNCrnHzrEw1oHufgnL/4.png')",
+    },
+    {
+      url: "/assets/images/5.png",
+      param:
+        "handleMint('https://magenta-multiple-leech-484.mypinata.cloud/ipfs/QmXZ3TgRgd5EZEk2DhwGvjf8f6sQJNCrnHzrEw1oHufgnL/5.png')",
+    },
+  ];
+
+  async function withdrawMoney() {
+    try {
+      const response = await NFTContract?.withdrawMoney();
+      console.log("Received: ", response);
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  if (!isCorrectNetwork) {
+    return (
+      <div className="flex min-h-[400px]  text-black flex-col items-center justify-between">
+        <br />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          NFT Marketplace
+        </h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          Switch to the Polygon Mumbai Network
+        </h2>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          Please switch to the Polygon Mumbai network to use this app.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+    );
+  }
+
+  async function handleMint(tokenURI: string) {
+    setIsMinting(true);
+    try {
+      const options = { value: ethers.utils.parseEther("0.01") };
+      const response = await NFTContract?.mintNFT(tokenURI, options);
+      console.log("Received: ", response);
+    } catch (err) {
+      alert(err);
+    } finally {
+      setIsMinting(false);
+    }
+  }
+
+  if (account === null) {
+    return (
+      <>
+        <div className="flex min-h-[400px]  text-black  flex-col items-center justify-between">
+          <br />
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            NFT Marketplace
+          </h1>
+          <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            Buy an NFT from our marketplace.
+          </h2>
+
+          {isWalletInstalled ? (
+            <Button
+              className="flex gap-1"
+              size={"lg"}
+              variant="outline"
+              onClick={connectWallet}
+            >
+              Connect Wallet
+              <Image
+                src={"/assets/metamask.svg"}
+                width={30}
+                height={30}
+                alt={""}
+              />
+            </Button>
+          ) : (
+            <p>Install Metamask wallet</p>
+          )}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <main className="flex  gap-10  flex-col items-center justify-between p-24">
+      <div className="flex text-center  flex-col">
+        <h1 className="scroll-m-20 text-4xl pb-5 font-extrabold tracking-tight lg:text-5xl">
+          NFT Marketplace
+        </h1>
+        <p className="pb-10">A NFT Marketplace to view and mint your NFT</p>
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          {data.map((item, index) => (
+            <div
+              key={item.param}
+              className="flex flex-col items-center justify-center gap-2"
+            >
+              <Image
+                src={item.url}
+                key={index}
+                className="rounded-md max-w-[200px]"
+                alt="images"
+                width={250}
+                height={250}
+              />
+              <Button
+                variant={"default"}
+                onClick={() => {
+                  handleMint(item.param);
+                }}
+              >
+                Mint - 0.01 MATIC
+              </Button>
+            </div>
+          ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex flex-col sm:flex-row gap-4">
+        <Button
+          variant={"secondary"}
+          className="h-[70px] sm:h-11"
+          onClick={() => {
+            withdrawMoney();
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Withdraw Money from Contract
+        </Button>
+        <Button variant={"destructive"} onClick={disconnectWallet}>
+          Disconnect Wallet
+        </Button>
       </div>
     </main>
-  )
+  );
 }
